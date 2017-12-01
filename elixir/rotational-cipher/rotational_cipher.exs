a4366a9f905a4339efe7177ca731664046895a2a
k-hamada <k.hamada.1988@gmail.com>
2017-12-01 21:42:05 +0900

    [elixir] rotational-cipher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -8,6 +8,21 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
+    String.codepoints(text)
+    |> Enum.map(&(char_rotate(&1, shift)))
+    |> List.to_string
+  end
+
+  def char_rotate(char, shift) do
+    cond do
+      char =~ ~r/[A-Z]/ -> char_rotate(char, shift, 65)
+      char =~ ~r/[a-z]/ -> char_rotate(char, shift, 97)
+      true -> char
+    end
+  end
+
+  def char_rotate(char, shift, basecodepoint) do
+    <<eacute :: utf8>> = char
+    <<basecodepoint + Integer.mod(eacute - basecodepoint + shift, 26)>>
   end
 end
-
