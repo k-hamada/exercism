de0bbd0c7142bfbb37d2a233a3a08ce7594cf28e
k.hamada <k.hamada.1988@gmail.com>
2017-12-03 14:24:23 +0900

    [go] bob

--- go/bob/bob.go
+++ go/bob/bob.go
@@ -1,15 +1,40 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package bob should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
 package bob
 
-// Hey should have a comment documenting it.
+import (
+	"regexp"
+	"strings"
+)
+
+func say(s string) bool {
+	return len(s) == 0
+}
+
+func yell(s string) bool {
+	re := regexp.MustCompile("[A-Za-z]+")
+	ws := re.FindAllString(s, -1)
+	y := len(ws) != 0
+	for _, w := range ws {
+		y = y && strings.ToUpper(w) == w
+	}
+	return y
+}
+
+func ask(s string) bool {
+	return strings.HasSuffix(s, "?")
+}
+
+// Hey bob
 func Hey(remark string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
-	return ""
+	remark = strings.TrimSpace(remark)
+
+	switch {
+	case say(remark):
+		return "Fine. Be that way!"
+	case yell(remark):
+		return "Whoa, chill out!"
+	case ask(remark):
+		return "Sure."
+	default:
+		return "Whatever."
+	}
 }
