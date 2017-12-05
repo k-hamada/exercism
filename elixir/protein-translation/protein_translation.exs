47721a7165885f762336ab0787c86bd56d6866f0
k-hamada <k.hamada.1988@gmail.com>
2017-12-05 21:51:21 +0900

    [elixir] protein-translation

--- elixir/protein-translation/protein_translation.exs
+++ elixir/protein-translation/protein_translation.exs
@@ -1,9 +1,41 @@
 defmodule ProteinTranslation do
+  @codon_map %{
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UGG" => "Tryptophan",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
   @doc """
   Given an RNA string, return a list of proteins specified by codons, in order.
   """
   @spec of_rna(String.t()) :: { atom,  list(String.t()) }
   def of_rna(rna) do
+    codons = rna
+    |> to_codons
+    |> Enum.map(&(@codon_map[&1]))
+    |> Enum.take_while(fn (codon) -> codon != "STOP" end)
+    |> Enum.reject(&is_nil/1)
+    unless(Enum.empty?(codons), [do: {:ok, codons}, else: {:error, "invalid RNA"}])
+  end
+
+  defp to_codons(rna) do
+    String.to_charlist(rna)
+    |> Enum.chunk_every(3, 3, :discard)
+    |> Enum.map(&List.to_string/1)
   end
 
   @doc """
@@ -29,6 +61,9 @@ defmodule ProteinTranslation do
   """
   @spec of_codon(String.t()) :: { atom, String.t() }
   def of_codon(codon) do
+    case Map.fetch(@codon_map, codon) do
+      :error -> { :error, "invalid codon" }
+      value -> value
+    end
   end
 end
-
