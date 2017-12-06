3446f33d98644ab9a150ae7c2eb4ba3abe6f3ce7
k.hamada <k.hamada.1988@gmail.com>
2017-12-07 00:11:18 +0900

    [go] hamming

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,4 +1,26 @@
 package hamming
 
+import (
+	"fmt"
+)
+
+// Distance : Hammint distance
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return -1, fmt.Errorf("invalid strings len(a) == %d, len(b) == %d", len(a), len(b))
+	}
+
+	if a == b {
+		return 0, nil
+	}
+
+	var res int
+	var rb = []rune(b)
+	for i, ar := range a {
+		if rb[i] != ar {
+			res++
+		}
+	}
+
+	return res, nil
 }
