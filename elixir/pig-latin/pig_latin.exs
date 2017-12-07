e0834440bc87b005e69e64bea0ff5b283cbfc411
k-hamada <k.hamada.1988@gmail.com>
2017-12-07 21:05:21 +0900

    [elixir] pig-latin

--- elixir/pig-latin/pig_latin.exs
+++ elixir/pig-latin/pig_latin.exs
@@ -15,6 +15,21 @@ defmodule PigLatin do
   """
   @spec translate(phrase :: String.t()) :: String.t()
   def translate(phrase) do
+    phrase
+    |> String.split( " ")
+    |> Enum.map(&translate_word/1)
+    |> Enum.join(" ")
+  end
+
+  defp translate_word(word) do
+    matcher = ~r{a|i|u(?!a|i|e|o)|e|o|yt|xr}
+    pigword =
+      unless String.first(word) |> String.match?(matcher) do
+        [x, y, z] = String.split(word, matcher, parts: 2, include_captures: true)
+        y <> z <> x
+      else
+        word
+      end
+    pigword <> "ay"
   end
 end
-
