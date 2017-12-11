4df98e332cef52f5c7229c543bf557591bc4e159
k.hamada <k.hamada.1988@gmail.com>
2017-12-12 00:05:57 +0900

    [go] clock minute only

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,47 +2,38 @@ package clock
 
 import "fmt"
 
-// Clock hour and minute
+// Clock have minutes
 type Clock struct {
-	Hour   int
-	Minute int
+	minutes int
 }
 
-func calcDiffHourAndFixedMinute(minute int) (diffHour, fixedMinute int) {
-	if minute >= 0 {
-		diffHour = minute / 60
-		fixedMinute = minute % 60
-	} else if (minute % -60) == 0 {
-		diffHour = -(minute / -60)
-		fixedMinute = 0
-	} else {
-		diffHour = -1 - (minute / -60)
-		fixedMinute = 60 + minute%60
-	}
-	return
-}
-
-func calcFixedHour(hour, diffHour int) (fixedHour int) {
-	fixedHour = hour + diffHour
-	if fixedHour < 0 {
-		fixedHour = 24 + fixedHour%24
-	}
-	return fixedHour % 24
-}
+// MinutesPerDay 1 day == 24 hour * 60 minute
+const MinutesPerDay = 24 * 60
 
 // New is constructor
 func New(hour, minute int) Clock {
-	diffHour, fixedMinute := calcDiffHourAndFixedMinute(minute)
-	fixedHour := calcFixedHour(hour, diffHour)
-	return Clock{Hour: fixedHour, Minute: fixedMinute}
+	newMinute := hour*60 + minute
+	for newMinute < 0 {
+		newMinute += MinutesPerDay
+	}
+	newMinute %= MinutesPerDay
+	return Clock{minutes: newMinute}
+}
+
+func (c Clock) hour() int {
+	return c.minutes / 60 % 24
+}
+
+func (c Clock) minute() int {
+	return c.minutes % 60
 }
 
 // String is stringer
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.Hour, c.Minute)
+	return fmt.Sprintf("%02d:%02d", c.hour(), c.minute())
 }
 
 // Add is calculation
 func (c Clock) Add(minutes int) Clock {
-	return New(c.Hour, c.Minute+minutes)
+	return New(c.hour(), c.minute()+minutes)
 }
