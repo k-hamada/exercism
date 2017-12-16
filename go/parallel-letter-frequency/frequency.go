77882c84be86916bcddeae7092af673a43f6cf6f
k.hamada <k.hamada.1988@gmail.com>
2017-12-16 17:00:36 +0900

    [go] parallel-letter-frequency

--- go/parallel-letter-frequency/frequency.go
+++ go/parallel-letter-frequency/frequency.go
@@ -9,3 +9,33 @@ func Frequency(s string) FreqMap {
 	}
 	return m
 }
+
+func frequencies(ss []string) <-chan FreqMap {
+	out := make(chan FreqMap, len(ss))
+	go func() {
+		defer close(out)
+		for _, s := range ss {
+			out <- Frequency(s)
+		}
+	}()
+	return out
+}
+
+func sum(in <-chan FreqMap) <-chan FreqMap {
+	out := make(chan FreqMap, 1)
+	go func() {
+		defer close(out)
+		res := FreqMap{}
+		for f := range in {
+			for k, v := range f {
+				res[k] += v
+			}
+		}
+		out <- res
+	}()
+	return out
+}
+
+func ConcurrentFrequency(ss []string) FreqMap {
+	return <-sum(frequencies(ss))
+}
