8bbe22bdcb5a39a292c207201b9f7a6c4893283d
k.hamada <k.hamada.1988@gmail.com>
2017-12-16 19:09:23 +0900

    [elixir] bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,27 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        true -> raise "Your implementation goes here"
+      say?(input)  -> "Fine. Be that way!"
+      ask?(input)  -> "Sure."
+      yell?(input) -> "Whoa, chill out!"
+      true         -> "Whatever."
+    end
+  end
 
+  defp say?(input) do
+    String.trim(input) == ""
+  end
+
+  defp ask?(input) do
+    String.ends_with?(input, "?")
+  end
+
+  defp yell?(input) do
+    words = String.split(input, ~r/[[:digit:][:punct:][:space:]]/, trim: true)
+    unless Enum.empty?(words) do
+      words |> Enum.all?(&(&1 == String.upcase(&1)))
+    else
+      false
     end
   end
 end
