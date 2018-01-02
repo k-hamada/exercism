43324d59391755dcb53d3dd2ee7c1d44ad394f36
k.hamada <k.hamada.1988@gmail.com>
2018-01-03 01:10:39 +0900

    [elixir] sublist

--- elixir/sublist/sublist.exs
+++ elixir/sublist/sublist.exs
@@ -5,35 +5,33 @@ defmodule Sublist do
   """
   def compare(a, b) do
     cond do
-      is_equal(a, b) -> :equal
-      is_sublist(a, b) -> :sublist
-      is_superlist(a, b) -> :superlist
+      is_equal?(a, b) -> :equal
+      is_sublist?(a, b) -> :sublist
+      is_superlist?(a, b) -> :superlist
       true -> :unequal
     end
   end
 
-  defp is_equal(a, b) do
+  defp is_equal?(a, b) do
     a === b
   end
 
-  defp is_sublist([], _), do: true
-  defp is_sublist(_, []), do: false
-  defp is_sublist(a, b) do
-    index = Enum.find_index(b, fn(e) -> e === hd(a) end)
-    size = Enum.count(a)
-    is_sublist(a, b, index, size)
+  defp is_sublist?([], _), do: true
+  defp is_sublist?(_, []), do: false
+  defp is_sublist?(a, b) do
+    is_sublist?(a, b, Enum.find_index(b, fn(e) -> e === hd(a) end))
   end
 
-  defp is_sublist(_, _, nil, _), do: false
-  defp is_sublist(a, b, index, size) do
-    if is_equal(a, b |> Enum.drop(index) |> Enum.take(size)) do
+  defp is_sublist?(_, _, nil), do: false
+  defp is_sublist?(a, b, index) do
+    if b |> Enum.drop(index) |> List.starts_with?(a) do
       true
     else
-      is_sublist(a, tl(b))
+      is_sublist?(a, tl(b))
     end
   end
 
-  defp is_superlist(a, b) do
-    is_sublist(b, a)
+  defp is_superlist?(a, b) do
+    is_sublist?(b, a)
   end
 end
