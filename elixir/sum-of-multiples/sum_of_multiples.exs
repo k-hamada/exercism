21942cb24fca09203204fb2f7c775d4f945b5cce
k.hamada <k.hamada.1988@gmail.com>
2018-01-03 04:28:04 +0900

    [elixir] sum-of-multiples

--- elixir/sum-of-multiples/sum_of_multiples.exs
+++ elixir/sum-of-multiples/sum_of_multiples.exs
@@ -4,6 +4,12 @@ defmodule SumOfMultiples do
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
   def to(limit, factors) do
-
+    sequence = Stream.iterate(1, &(&1 + 1))
+    factors
+    |> Enum.map(fn(factor) -> Stream.map(sequence, &(factor * &1)) end)
+    |> Enum.map(fn(stream) -> Stream.take_while(stream, &(limit > &1)) end)
+    |> Stream.concat
+    |> Stream.uniq
+    |> Enum.sum
   end
 end
