133b0e07b67ff1cf8ee1a9a62347d3871e5741b5
k.hamada <k.hamada.1988@gmail.com>
2018-01-06 00:34:27 +0900

    [elixir] anagram

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -4,6 +4,27 @@ defmodule Anagram do
   """
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
+    candidates
+    |> Enum.reject(same_word(base))
+    |> Enum.filter(match_word(base))
+  end
 
+  defp same_word(base) do
+    fn (candidate) ->
+      String.downcase(base) === String.downcase(candidate)
+    end
+  end
+
+  defp match_word(base) do
+    fn (candidate) ->
+      letter_sort(base) === letter_sort(candidate)
+    end
+  end
+
+  defp letter_sort(word) do
+    word
+    |> String.downcase
+    |> String.graphemes
+    |> Enum.sort
   end
 end
