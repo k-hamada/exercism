c7b020ddc00303b8a625a0f6dbd5691a7c7a57a0
k.hamada <k.hamada.1988@gmail.com>
2018-01-13 03:32:02 +0900

    [elixir] all-your-base

--- elixir/all-your-base/all-your-base.exs
+++ elixir/all-your-base/all-your-base.exs
@@ -3,8 +3,34 @@ defmodule AllYourBase do
   Given a number in base a, represented as a sequence of digits, converts it to base b,
   or returns nil if either of the bases are less than 2
   """
-
   @spec convert(list, integer, integer) :: list
   def convert(digits, base_a, base_b) do
+    cond do
+      is_fail?(digits, base_a, base_b) ->
+        nil
+      true ->
+        to_base_n(to_base_ten(digits, base_a), base_b, [])
+    end
+  end
+
+  defp is_fail?(digits, base_a, base_b) do
+    digits == [] ||
+    base_a <= 1 ||
+    base_b <= 1 ||
+    Enum.any?(digits, &(&1 < 0 || &1 >= base_a))
+  end
+
+  defp to_base_ten(digits, base) do
+    digits
+    |> Enum.reverse
+    |> Enum.with_index
+    |> Enum.map(fn {x, y} -> round(:math.pow(base, y) * x) end)
+    |> Enum.sum
+  end
+
+  defp to_base_n(0, _, []), do: [0]
+  defp to_base_n(0, _, acc), do: acc
+  defp to_base_n(number, base, acc) do
+    to_base_n(div(number, base), base, [rem(number, base) | acc])
   end
 end
