448a5fbea4850bb352c6c10d0546f5129437512a
k.hamada <k.hamada.1988@gmail.com>
2018-01-13 00:54:36 +0900

    [elixir] roman-numerals

--- elixir/roman-numerals/roman.exs
+++ elixir/roman-numerals/roman.exs
@@ -4,16 +4,16 @@ defmodule Roman do
   """
   @spec numerals(pos_integer) :: String.t
   def numerals(number) do
-    romanize(div(number, 1000),         %{:_1 => "M", :_5 => "",  :_a => "" }) <>
-    romanize(div(number, 100),          %{:_1 => "C", :_5 => "D", :_a => "M"}) <>
-    romanize(div(rem(number, 100), 10), %{:_1 => "X", :_5 => "L", :_a => "C"}) <>
-    romanize(rem(number, 10),           %{:_1 => "I", :_5 => "V", :_a => "X"})
+    romanize(rem(div(number, 1000), 10), %{:one => "M", :five => "",  :ten => "" }) <>
+    romanize(rem(div(number, 100), 10),  %{:one => "C", :five => "D", :ten => "M"}) <>
+    romanize(rem(div(number, 10), 10),   %{:one => "X", :five => "L", :ten => "C"}) <>
+    romanize(rem(div(number, 1), 10),    %{:one => "I", :five => "V", :ten => "X"})
   end
 
-  defp romanize(n, table) when n in 1..3, do: String.duplicate(table[:_1], n)
-  defp romanize(n, table) when n in 4..4, do: table[:_1] <> table[:_5]
-  defp romanize(n, table) when n in 5..5, do: table[:_5]
-  defp romanize(n, table) when n in 6..8, do: table[:_5] <> String.duplicate(table[:_1], n - 5)
-  defp romanize(n, table) when n in 9..9, do: table[:_1] <> table[:_a]
+  defp romanize(n, table) when n in 1..3, do: String.duplicate(table[:one], n)
+  defp romanize(n, table) when n in 4..4, do: table[:one] <> table[:five]
+  defp romanize(n, table) when n in 5..5, do: table[:five]
+  defp romanize(n, table) when n in 6..8, do: table[:five] <> String.duplicate(table[:one], n - 5)
+  defp romanize(n, table) when n in 9..9, do: table[:one] <> table[:ten]
   defp romanize(_, _), do: ""
 end
