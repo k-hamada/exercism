5465df4805ca2470d9894595d2cd74abeee819b6
k.hamada <k.hamada.1988@gmail.com>
2018-01-14 00:09:10 +0900

    [elixir] isogram

--- elixir/isogram/isogram.exs
+++ elixir/isogram/isogram.exs
@@ -4,7 +4,17 @@ defmodule Isogram do
   """
   @spec isogram?(String.t) :: boolean
   def isogram?(sentence) do
-
+    graphemes =
+      sentence
+      |> String.graphemes
+      |> Enum.reject(&(&1 in ["-", " "]))
+    count =
+      graphemes
+      |> Enum.count
+    uniq_count =
+      graphemes
+      |> Enum.uniq
+      |> Enum.count
+    count == uniq_count
   end
-
 end
