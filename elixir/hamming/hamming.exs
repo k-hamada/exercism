a282259263945952af0f647b1b0c35e413938b43
k.hamada <k.hamada.1988@gmail.com>
2018-01-13 17:54:52 +0900

    [elixir] hamming

--- elixir/hamming/hamming.exs
+++ elixir/hamming/hamming.exs
@@ -8,6 +8,10 @@ defmodule Hamming do
   {:ok, 4}
   """
   @spec hamming_distance([char], [char]) :: non_neg_integer
+  def hamming_distance(strand1, strand2) when length(strand1) != length(strand2) do
+    {:error, "Lists must be the same length"}
+  end
   def hamming_distance(strand1, strand2) do
+    {:ok, Enum.count(List.zip([strand1, strand2]), fn {a, b} -> a != b end)}
   end
 end
