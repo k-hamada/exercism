f78ea95fac0e85221722321cd969b9f5d9d60a0c
k.hamada <k.hamada.1988@gmail.com>
2018-01-13 23:25:04 +0900

    [elixir] beer-song

--- elixir/beer-song/beer_song.exs
+++ elixir/beer-song/beer_song.exs
@@ -3,33 +3,21 @@ defmodule BeerSong do
   Get a single verse of the beer song
   """
   @spec verse(integer) :: String.t
-  def verse(number) when number in 99..3 do
+  def verse(number) when number in 99..0 do
     """
-    #{number} bottles of beer on the wall, #{number} bottles of beer.
-    Take one down and pass it around, #{number - 1} bottles of beer on the wall.
-    """
-  end
-
-  def verse(2) do
-    """
-    2 bottles of beer on the wall, 2 bottles of beer.
-    Take one down and pass it around, 1 bottle of beer on the wall.
+    #{String.capitalize(bottle(number))} of beer on the wall, #{bottle(number)} of beer.
+    #{take(number)}, #{bottle(number - 1)} of beer on the wall.
     """
   end
 
-  def verse(1) do
-    """
-    1 bottle of beer on the wall, 1 bottle of beer.
-    Take it down and pass it around, no more bottles of beer on the wall.
-    """
-  end
+  defp bottle(-1), do: 99 |> bottle
+  defp bottle(0), do: "no more" |> bottle
+  defp bottle(1), do: "1 bottle"
+  defp bottle(number), do: "#{number} bottles"
 
-  def verse(0) do
-    """
-    No more bottles of beer on the wall, no more bottles of beer.
-    Go to the store and buy some more, 99 bottles of beer on the wall.
-    """
-  end
+  defp take(0), do: "Go to the store and buy some more"
+  defp take(1), do: 1 |> take("it")
+  defp take(_, it \\ "one"), do: "Take #{it} down and pass it around"
 
   @doc """
   Get the entire beer song for a given range of numbers of bottles.
@@ -37,7 +25,6 @@ defmodule BeerSong do
   @spec lyrics(Range.t) :: String.t
   def lyrics(range \\ 99..0) do
     range
-    |> Enum.map(&verse/1)
-    |> Enum.join("\n")
+    |> Enum.map_join("\n", &verse/1)
   end
 end
