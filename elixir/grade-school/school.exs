72b476bac2f6736c62616fbe3034573e0a6c1b86
k.hamada <k.hamada.1988@gmail.com>
2018-01-14 21:39:49 +0900

    [elixir] grade-school

--- elixir/grade-school/school.exs
+++ elixir/grade-school/school.exs
@@ -10,7 +10,7 @@ defmodule School do
   """
   @spec add(map, String.t, integer) :: map
   def add(db, name, grade) do
-
+    Map.put(db, grade, [name | grade(db, grade)])
   end
 
   @doc """
@@ -18,7 +18,7 @@ defmodule School do
   """
   @spec grade(map, integer) :: [String.t]
   def grade(db, grade) do
-
+    Map.get(db, grade, [])
   end
 
   @doc """
@@ -26,6 +26,8 @@ defmodule School do
   """
   @spec sort(map) :: [{integer, [String.t]}]
   def sort(db) do
-
+    Map.keys(db)
+    |> Enum.sort
+    |> Enum.map(&({&1, Map.get(db, &1) |> Enum.sort}))
   end
 end
