3da1f53a4bbc21bbb1074c9dced10e79c412c78b
k.hamada <k.hamada.1988@gmail.com>
2018-01-20 15:56:05 +0900

    [elixir] leap

--- elixir/leap/leap.exs
+++ elixir/leap/leap.exs
@@ -10,6 +10,11 @@ defmodule Year do
   """
   @spec leap_year?(non_neg_integer) :: boolean
   def leap_year?(year) do
-
+    leap_year_impl(year)
   end
+
+  defp leap_year_impl(y) when rem(y, 400) == 0, do: true
+  defp leap_year_impl(y) when rem(y, 100) == 0, do: false
+  defp leap_year_impl(y) when rem(y, 4) == 0, do: true
+  defp leap_year_impl(_), do: false
 end
