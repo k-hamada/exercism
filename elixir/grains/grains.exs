cc85572a4cef5817e4fb02d1772d64b53ff916d5
k.hamada <k.hamada.1988@gmail.com>
2018-01-21 15:40:19 +0900

    [elixir] grains

--- elixir/grains/grains.exs
+++ elixir/grains/grains.exs
@@ -1,10 +1,20 @@
 defmodule Grains do
+  use Bitwise, only_operators: true
+
   @doc """
   Calculate two to the power of the input minus one.
   """
   @spec square(pos_integer) :: pos_integer
   def square(number) do
+    square_impl(number)
+  end
 
+  defp square_impl(n) when 0 < n and n < 65 do
+    {:ok, 1 <<< (n - 1)}
+  end
+
+  defp square_impl(_) do
+    {:error, "The requested square must be between 1 and 64 (inclusive)"}
   end
 
   @doc """
@@ -12,6 +22,12 @@ defmodule Grains do
   """
   @spec total :: pos_integer
   def total do
+    sum =
+      1..64
+      |> Enum.map(&square/1)
+      |> Keyword.get_values(:ok)
+      |> Enum.sum()
 
+    {:ok, sum}
   end
 end
