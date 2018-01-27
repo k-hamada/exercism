2b11eade2cff4c4e5a27afc99bfa928ccc27b089
k.hamada <k.hamada.1988@gmail.com>
2018-01-28 01:39:08 +0900

    [elixir] binary

--- elixir/binary/binary.exs
+++ elixir/binary/binary.exs
@@ -6,18 +6,11 @@ defmodule Binary do
   """
   @spec to_decimal(String.t()) :: non_neg_integer
   def to_decimal(string) do
-    string
-    |> parse
-    |> Enum.reverse()
-    |> Enum.with_index()
-    |> Enum.map(&calc_decimal/1)
-    |> Enum.sum()
+    binary_to_decimal(string, 0)
   end
 
-  defp parse(binary) do
-    if String.match?(binary, ~r/^[0|1]+$/), do: binary |> String.graphemes(), else: ["0"]
-  end
-
-  defp calc_decimal({"0", _}), do: 0
-  defp calc_decimal({"1", i}), do: :math.pow(2, i)
+  defp binary_to_decimal("", acc), do: acc
+  defp binary_to_decimal("0" <> tail, acc), do: binary_to_decimal(tail, acc * 2)
+  defp binary_to_decimal("1" <> tail, acc), do: binary_to_decimal(tail, acc * 2 + 1)
+  defp binary_to_decimal(_, _), do: 0
 end
