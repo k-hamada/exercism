926bc74ac01db7747caa119144d1a5668f8f6942
k.hamada <k.hamada.1988@gmail.com>
2018-02-03 22:27:17 +0900

    [elixir] luhn

--- elixir/luhn/luhn.exs
+++ elixir/luhn/luhn.exs
@@ -1,45 +1,41 @@
 defmodule Luhn do
+  import Integer, only: [is_odd: 1]
+
   @doc """
   Checks if the given number is valid via the luhn formula
   """
   @spec valid?(String.t()) :: boolean
   def valid?(number) do
-    number |> parse |> check
+    with true <- has_many_digits?(number),
+         true <- only_digit_or_space?(number),
+         do: number |> parse |> check == 0
+  end
+
+  defp has_many_digits?(number) do
+    Regex.scan(~r/\d/, number) |> Enum.count() > 1
+  end
+
+  defp only_digit_or_space?(number) do
+    not String.match?(number, ~r/[^\d\s]/)
   end
 
   defp parse(number) do
-    if String.match?(number, ~r/[^\d|\s]/) do
-      []
-    else
-      number
-      |> String.graphemes()
-      |> Enum.reject(&(&1 == " "))
-      |> Enum.map(&String.to_integer/1)
-      |> Enum.reverse()
-    end
-  end
-
-  defp check(numbers) when length(numbers) <= 1 do
-    false
+    number
+    |> String.graphemes()
+    |> Enum.reject(&(&1 == " "))
+    |> Enum.map(&String.to_integer/1)
   end
 
   defp check(numbers) do
     numbers
+    |> Enum.reverse()
     |> Enum.with_index(1)
     |> Enum.flat_map(&calc/1)
     |> Enum.sum()
-    |> rem(10) == 0
+    |> rem(10)
   end
 
-  defp calc({n, i}) when rem(i, 2) != 0, do: [n]
-
-  defp calc({n, _}) do
-    res = n * 2
-
-    if res >= 10 do
-      [1, rem(res, 10)]
-    else
-      [res]
-    end
-  end
+  defp calc({n, i}) when is_odd(i), do: [n]
+  defp calc({n, _}) when n * 2 >= 10, do: [1, rem(n * 2, 10)]
+  defp calc({n, _}), do: [n * 2]
 end
