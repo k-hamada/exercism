6feeea5a0722f43943cbbe7a8e405375085bc057
k.hamada <k.hamada.1988@gmail.com>
2018-02-10 04:12:05 +0900

    [elixir] gigasecond

--- elixir/gigasecond/gigasecond.exs
+++ elixir/gigasecond/gigasecond.exs
@@ -2,17 +2,9 @@ defmodule Gigasecond do
   @doc """
   Calculate a date one billion seconds after an input date.
   """
-  @epoch ~N[1970-01-01 00:00:00]
   @spec from({{pos_integer, pos_integer, pos_integer}, {pos_integer, pos_integer, pos_integer}}) :: :calendar.datetime()
   def from(erl) do
     with {:ok, naive_date_time} <- NaiveDateTime.from_erl(erl),
-         do: naive_date_time |> add_gigasecond
-  end
-
-  defp add_gigasecond(naive_date_time) do
-    naive_date_time
-    |> NaiveDateTime.diff(@epoch)
-    |> (&NaiveDateTime.add(@epoch, &1 + 1_000_000_000)).()
-    |> NaiveDateTime.to_erl()
+         do: naive_date_time |> NaiveDateTime.add(1_000_000_000) |> NaiveDateTime.to_erl()
   end
 end
