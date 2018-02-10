e18f1af312c5caf351006d080899be860a468afc
k.hamada <k.hamada.1988@gmail.com>
2018-02-10 21:34:36 +0900

    [elixir] pascals-triangle

--- elixir/pascals-triangle/pascals_triangle.exs
+++ elixir/pascals-triangle/pascals_triangle.exs
@@ -5,5 +5,24 @@ defmodule PascalsTriangle do
   """
   @spec rows(integer) :: [[integer]]
   def rows(num) do
+    1..num
+    |> Enum.reduce([], &get_row/2)
+    |> Enum.reverse()
+  end
+
+  defp get_row(1, _) do
+    [[1]]
+  end
+
+  defp get_row(_, [head | tail]) do
+    [next_row(head) | [head | tail]]
+  end
+
+  defp next_row(list) do
+    list
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(&Enum.sum/1)
+    |> List.insert_at(0, 1)
+    |> List.insert_at(-1, 1)
   end
 end
