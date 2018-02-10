3be20be10a29519f2498588823d60c646f23b2e3
k.hamada <k.hamada.1988@gmail.com>
2018-02-10 18:39:34 +0900

    [elixir] queen-attack

--- elixir/queen-attack/queen_attack.exs
+++ elixir/queen-attack/queen_attack.exs
@@ -1,5 +1,5 @@
 defmodule Queens do
-  @type t :: %Queens{ black: {integer, integer}, white: {integer, integer} }
+  @type t :: %Queens{black: {integer, integer}, white: {integer, integer}}
   defstruct black: nil, white: nil
 
   @doc """
@@ -7,8 +7,16 @@ defmodule Queens do
   """
   @spec new() :: Queens.t()
   @spec new({integer, integer}, {integer, integer}) :: Queens.t()
-  def new(white, black) do
+  def new(white \\ {0, 3}, black \\ {7, 3}) do
+    new_queens(white, black)
+  end
 
+  defp new_queens(white, white) do
+    raise ArgumentError
+  end
+
+  defp new_queens(white, black) do
+    %Queens{white: white, black: black}
   end
 
   @doc """
@@ -17,7 +25,27 @@ defmodule Queens do
   """
   @spec to_string(Queens.t()) :: String.t()
   def to_string(queens) do
+    with board <- new_board(),
+         {_, board} <- set_to_board(board, queens.white, "W"),
+         {_, board} <- set_to_board(board, queens.black, "B"),
+         do: board |> board_to_string
+  end
 
+  defp set_to_board(board, {row, col}, symbol) do
+    Map.get_and_update(board, row, fn board_row ->
+      {board_row, board_row |> List.update_at(col, fn _ -> symbol end)}
+    end)
+  end
+
+  defp new_board() do
+    Enum.reduce(0..7, %{}, fn i, acc -> Map.put(acc, i, List.duplicate("_", 8)) end)
+  end
+
+  defp board_to_string(board) do
+    board
+    |> Map.values()
+    |> Enum.map(&Enum.join(&1, " "))
+    |> Enum.join("\n")
   end
 
   @doc """
@@ -25,6 +53,13 @@ defmodule Queens do
   """
   @spec can_attack?(Queens.t()) :: boolean
   def can_attack?(queens) do
+    can_attack?(queens.white, queens.black)
+  end
 
+  defp can_attack?({row, _}, {row, _}), do: true
+  defp can_attack?({_, col}, {_, col}), do: true
+
+  defp can_attack?({w_row, w_col}, {b_row, b_col}) do
+    abs(w_row - w_col) == abs(b_row - b_col)
   end
 end
--- elixir/queen-attack/queen_attack_test.exs
+++ elixir/queen-attack/queen_attack_test.exs
@@ -15,21 +15,18 @@ defmodule QueenAttackTest do
     assert queens.black == {7, 3}
   end
 
-  @tag :pending
   test "specific placement" do
     queens = Queens.new({3, 7}, {6, 1})
     assert queens.white == {3, 7}
     assert queens.black == {6, 1}
   end
 
-  @tag :pending
   test "cannot occupy same space" do
     assert_raise ArgumentError, fn ->
       Queens.new({2, 4}, {2, 4})
     end
   end
 
-  @tag :pending
   test "string representation" do
     queens = Queens.new({2, 4}, {6, 6})
 
@@ -48,7 +45,6 @@ defmodule QueenAttackTest do
     assert Queens.to_string(queens) == board
   end
 
-  @tag :pending
   test "another string representation" do
     queens = Queens.new({7, 1}, {0, 0})
 
@@ -67,7 +63,6 @@ defmodule QueenAttackTest do
     assert Queens.to_string(queens) == board
   end
 
-  @tag :pending
   test "yet another string representation" do
     queens = Queens.new({4, 3}, {3, 4})
 
@@ -86,7 +81,6 @@ defmodule QueenAttackTest do
     assert Queens.to_string(queens) == board
   end
 
-  @tag :pending
   test "queen placed on the bottom right corner" do
     queens = Queens.new({4, 3}, {7, 7})
 
@@ -105,7 +99,6 @@ defmodule QueenAttackTest do
     assert Queens.to_string(queens) == board
   end
 
-  @tag :pending
   test "queen placed on the edge of the board" do
     queens = Queens.new({4, 3}, {2, 7})
 
@@ -124,43 +117,36 @@ defmodule QueenAttackTest do
     assert Queens.to_string(queens) == board
   end
 
-  @tag :pending
   test "cannot attack" do
     queens = Queens.new({2, 3}, {4, 7})
     refute Queens.can_attack?(queens)
   end
 
-  @tag :pending
   test "can attack on same row" do
     queens = Queens.new({2, 4}, {2, 7})
     assert Queens.can_attack?(queens)
   end
 
-  @tag :pending
   test "can attack on same column" do
     queens = Queens.new({5, 4}, {2, 4})
     assert Queens.can_attack?(queens)
   end
 
-  @tag :pending
   test "can attack on diagonal" do
     queens = Queens.new({1, 1}, {6, 6})
     assert Queens.can_attack?(queens)
   end
 
-  @tag :pending
   test "can attack on other diagonal" do
     queens = Queens.new({0, 6}, {1, 7})
     assert Queens.can_attack?(queens)
   end
 
-  @tag :pending
   test "can attack on yet another diagonal" do
     queens = Queens.new({4, 1}, {6, 3})
     assert Queens.can_attack?(queens)
   end
 
-  @tag :pending
   test "can attack on a diagonal slanted the other way" do
     queens = Queens.new({6, 1}, {1, 6})
     assert Queens.can_attack?(queens)
