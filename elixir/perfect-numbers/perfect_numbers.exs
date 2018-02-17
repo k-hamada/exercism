2e56767a55ce79c47dfa2c76f475135545d96c54
k.hamada <k.hamada.1988@gmail.com>
2018-02-17 22:34:14 +0900

    [elixir] perfect-numbers

--- elixir/perfect-numbers/perfect_numbers.exs
+++ elixir/perfect-numbers/perfect_numbers.exs
@@ -9,8 +9,38 @@ defmodule PerfectNumbers do
   :abundant if the aliquot sum is greater than `number`
   :deficient if the aliquot sum is less than `number`
   """
-  @spec classify(number :: integer) :: ({ :ok, atom } | { :error, String.t() })
+  @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
   def classify(number) do
+    with number when number > 0 <- number,
+         class <- number |> aliquot_sum |> placement(number) do
+      {:ok, class}
+    else
+      _ -> {:error, "Classification is only possible for natural numbers."}
+    end
+  end
+
+  defp placement(aliquot_sum, number) do
+    cond do
+      aliquot_sum == number -> :perfect
+      aliquot_sum > number -> :abundant
+      aliquot_sum < number -> :deficient
+    end
+  end
+
+  defp aliquot_sum(number) when number <= 0, do: 0
+
+  defp aliquot_sum(number) do
+    number
+    |> divisor
+    |> Enum.sum()
+  end
+
+  defp divisor(1), do: []
+  defp divisor(2), do: [1]
+
+  defp divisor(number) do
+    2..(div(number, 2) + 1)
+    |> Enum.filter(&(rem(number, &1) == 0))
+    |> Enum.concat([1])
   end
 end
-
