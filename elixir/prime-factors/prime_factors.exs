daa284779a05c3601845bf93da6007a525f6dfa5
k.hamada <k.hamada.1988@gmail.com>
2018-02-17 18:17:02 +0900

    [elixir] prime-factors
    via. https://wiki.haskell.org/Testing_primality#Primality_Test_and_Integer_Factorization

--- elixir/prime-factors/prime_factors.exs
+++ elixir/prime-factors/prime_factors.exs
@@ -9,20 +9,26 @@ defmodule PrimeFactors do
   """
   @spec factors_for(pos_integer) :: [pos_integer]
   def factors_for(number) do
-    prime_factorization(number, 2, [])
-    |> Enum.sort()
+    do_factors_for({2, number})
   end
 
-  defp prime_factorization(number, i, primes) do
-    cond do
-      number < i ->
-        primes
+  defp do_factors_for({d, n}) do
+    Stream.unfold({d, n}, fn
+      nil ->
+        nil
 
-      Integer.mod(number, i) == 0 ->
-        prime_factorization(div(number, i), i, [i | primes])
-
-      true ->
-        prime_factorization(number, i + 1, primes)
-    end
+      {d, n} ->
+        Stream.iterate(d, &(&1 + 1))
+        |> Stream.take_while(&(:math.pow(&1, 2) <= n))
+        |> Stream.concat(for i <- [n], i > 1, do: i)
+        |> Stream.filter(&(Integer.mod(n, &1) == 0))
+        |> Stream.map(&{&1, {&1, div(n, &1)}})
+        |> Enum.take(1)
+        |> list_to_maybe
+    end)
+    |> Enum.to_list()
   end
+
+  defp list_to_maybe([]), do: nil
+  defp list_to_maybe(list), do: list |> List.first()
 end
