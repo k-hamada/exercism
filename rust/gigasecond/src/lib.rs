4326523430980042d40438b9461350b1d99d4fcc
k.hamada <k.hamada.1988@gmail.com>
2018-02-25 02:13:34 +0900

    [rust] gigasecond

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -3,5 +3,5 @@ use chrono::*;
 
 // Returns a Utc DateTime one billion seconds after start.
 pub fn after(start: DateTime<Utc>) -> DateTime<Utc> {
-    unimplemented!()
+    start + Duration::seconds(1_000_000_000)
 }
