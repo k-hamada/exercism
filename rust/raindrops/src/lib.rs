359687ce72bb637c0f287ae24a0c196da1c6dcc2
k.hamada <k.hamada.1988@gmail.com>
2018-02-25 16:23:05 +0900

    [rust] raindrops

--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,3 +1,18 @@
 pub fn raindrops(n: usize) -> String {
-    unimplemented!()
+    let mut res = String::new();
+
+    if n % 3 == 0 {
+        res += "Pling"
+    }
+    if n % 5 == 0 {
+        res += "Plang"
+    }
+    if n % 7 == 0 {
+        res += "Plong"
+    }
+    if res == "" {
+        res += &n.to_string()
+    }
+
+    res
 }
