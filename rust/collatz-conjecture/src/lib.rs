79f05e96a35ebd803d130441cc0ea848d857bf6b
k.hamada <k.hamada.1988@gmail.com>
2018-03-18 18:00:37 +0900

    [rust] collatz-conjecture

--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -1,14 +1,13 @@
 // return Some(x) where x is the number of steps required to reach 1
 pub fn collatz(n: u64) -> Option<u64> {
-    match n {
-        0 => None,
-        _ => Some(do_collatz(n, 0))
-    }
+    do_collatz(n, 0)
 }
 
-fn do_collatz(n: u64, count: u64) -> u64 {
-    if n == 1 { return count; }
-
-    let m = if n % 2 == 0 { n / 2 } else { n * 3 + 1 };
-    do_collatz(m, count + 1)
+fn do_collatz(n: u64, count: u64) -> Option<u64> {
+    match (n, n % 2) {
+        (0, _) => None,
+        (1, _) => Some(count),
+        (i, 0) => do_collatz(i / 2, count + 1),
+        (i, _) => do_collatz(i * 3 + 1, count + 1)
+    }
 }
