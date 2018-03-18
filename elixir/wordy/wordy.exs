3dfde0f31b316df45a49e1ac500559dfaad43423
k.hamada <k.hamada.1988@gmail.com>
2018-03-18 19:21:16 +0900

    [elixir] wordy

--- elixir/wordy/wordy.exs
+++ elixir/wordy/wordy.exs
@@ -4,5 +4,46 @@ defmodule Wordy do
   """
   @spec answer(String.t()) :: integer
   def answer(question) do
+    Regex.scan(~r/\w+|\-?\d+|\?/, question)
+    |> List.flatten()
+    |> do_answer()
+  end
+
+  def do_answer(["What", "is" | question]) do
+    question
+    |> do_answer()
+  end
+
+  def do_answer([n, "plus", m | question]) do
+    [calc(:+, n, m) | question]
+    |> do_answer()
+  end
+
+  def do_answer([n, "minus", m | question]) do
+    [calc(:-, n, m) | question]
+    |> do_answer()
+  end
+
+  def do_answer([n, "multiplied", "by", m | question]) do
+    [calc(:*, n, m) | question]
+    |> do_answer()
+  end
+
+  def do_answer([n, "divided", "by", m | question]) do
+    [calc(:div, n, m) | question]
+    |> do_answer()
+  end
+
+  def do_answer([n, "?"]) do
+    n |> String.to_integer()
+  end
+
+  def do_answer(_) do
+    raise ArgumentError
+  end
+
+  def calc(op, n, m) do
+    apply(Kernel, op, [n, m] |> Enum.map(&String.to_integer/1))
+    |> to_string()
   end
 end
