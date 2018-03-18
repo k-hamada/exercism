c6945a2e6b597f02db41177699e7a9d8f445f169
k.hamada <k.hamada.1988@gmail.com>
2018-03-18 12:13:38 +0900

    [rust] pythagorean-triplet

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,12 @@
 pub fn find() -> Option<u32> {
-    unimplemented!();
+    const N : u32 = 1000;
+    for a in 1..N {
+        for b in a..N-a {
+            let c = N - a - b;
+            if (a*a + b*b == c*c) {
+                return Some(a * b * c)
+            }
+        }
+    }
+    return None
 }
