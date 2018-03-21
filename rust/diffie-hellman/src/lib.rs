b90a8e945408a426190e6478d8c5d50813bc228a
k.hamada <k.hamada.1988@gmail.com>
2018-03-21 14:03:07 +0900

    [rust] diffie-hellman

--- rust/diffie-hellman/src/lib.rs
+++ rust/diffie-hellman/src/lib.rs
@@ -1,11 +1,32 @@
+extern crate num_bigint;
+extern crate num_traits;
+extern crate primal_check;
+extern crate rand;
+
+use num_bigint::ToBigInt;
+use num_traits::cast::ToPrimitive;
+use primal_check::miller_rabin as is_prime;
+use rand::{thread_rng, Rng};
+
 pub fn private_key(p: u64) -> u64 {
-    unimplemented!("Pick a private key greater than 1 and less than {}", p)
+    let mut rng = thread_rng();
+    loop {
+        let k = rng.gen_range(2, p);
+        if is_prime(k) { return k; }
+    }
 }
 
 pub fn public_key(p: u64, g: u64, a: u64) -> u64 {
-    unimplemented!("Calculate public key using prime numbers {} and {}, and private key {}", p, g, a)
+    modpow(g, a, p)
 }
 
 pub fn secret(p: u64, b_pub: u64, a: u64) -> u64 {
-    unimplemented!("Calculate secret key using prime number {}, public key {}, and private key {}", p, b_pub, a)
+    modpow(b_pub, a, p)
+}
+
+fn modpow(base: u64, exponent: u64, modulus: u64) -> u64 {
+    let b = ToBigInt::to_bigint(&base).unwrap();
+    let e = ToBigInt::to_bigint(&exponent).unwrap();
+    let m = ToBigInt::to_bigint(&modulus).unwrap();
+    b.modpow(&e, &m).to_u64().unwrap()
 }
