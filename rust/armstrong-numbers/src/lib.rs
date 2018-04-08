6ce282f5c88f30909c88d006da60ec7d4d26a426
k.hamada <k.hamada.1988@gmail.com>
2018-04-08 22:11:59 +0900

    [rust] armstrong-numbers

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,3 +1,5 @@
-pub fn is_armstrong_number(_num: u32) -> bool {
-    unimplemented!()
+pub fn is_armstrong_number(num: u32) -> bool {
+    let number = num.to_string();
+    let len = number.len() as u32;
+    num == number.chars().map(|i| i.to_digit(10).unwrap().pow(len)).sum()
 }
