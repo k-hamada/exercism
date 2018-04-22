5faa480de7d8038eca7a308a70ee9ff8b4ce8cf4
k.hamada <k.hamada.1988@gmail.com>
2018-04-22 20:36:35 +0900

    [rust] simple-linked-list

--- rust/simple-linked-list/src/lib.rs
+++ rust/simple-linked-list/src/lib.rs
@@ -1,46 +1,89 @@
 pub struct SimpleLinkedList<T> {
-    // Delete this field
-    // dummy is needed to avoid unused parameter error during compilation
-    dummy: ::std::marker::PhantomData<T>,
+    head: Option<Box<Node<T>>>,
+    len: usize,
 }
+
+struct Node<T> {
+    element: T,
+    next: Option<Box<Node<T>>>,
+}
+
+pub struct IntoIter<T>(SimpleLinkedList<T>);
 
 impl<T> SimpleLinkedList<T> {
     pub fn new() -> Self {
-        unimplemented!()
+        SimpleLinkedList::<T> { head: None, len: 0 }
     }
 
     pub fn len(&self) -> usize {
-        unimplemented!()
+        self.len
     }
 
-    pub fn push(&mut self, _element: T) {
-        unimplemented!()
+    pub fn push(&mut self, element: T) {
+        self.len += 1;
+        self.head = Some(Box::new(Node::new(element, self.head.take())))
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        unimplemented!()
+        self.head.take().map(|node| {
+            self.len -= 1;
+            let node = *node;
+            self.head = node.next;
+            node.element
+        })
     }
 
     pub fn peek(&self) -> Option<&T> {
-        unimplemented!()
+        self.head.as_ref().map(|node| &node.element)
+    }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
     }
 }
 
 impl<T: Clone> SimpleLinkedList<T> {
     pub fn rev(&self) -> SimpleLinkedList<T> {
-        unimplemented!()
+        let mut list: SimpleLinkedList<T> = SimpleLinkedList::new();
+        let mut current = self.head.as_ref();
+        while let Some(node) = current {
+            list.push(node.element.clone());
+            current = node.next.as_ref();
+        }
+        list
     }
 }
 
-
 impl<'a, T: Clone> From<&'a [T]> for SimpleLinkedList<T> {
-    fn from(_item: &[T]) -> Self {
-        unimplemented!()
+    fn from(items: &[T]) -> Self {
+        let mut list: SimpleLinkedList<T> = SimpleLinkedList::new();
+        for item in items {
+            list.push(item.clone());
+        }
+        list
     }
 }
 
 impl<T> Into<Vec<T>> for SimpleLinkedList<T> {
     fn into(self) -> Vec<T> {
-        unimplemented!()
+        let mut vec = self.into_iter().collect::<Vec<_>>();
+        vec.reverse();
+        vec
+    }
+}
+
+impl<T> Node<T> {
+    pub fn new(element: T, next: Option<Box<Node<T>>>) -> Self {
+        Node::<T> {
+            element: element,
+            next: next,
+        }
     }
 }
