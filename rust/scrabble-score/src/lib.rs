8adfaf99778617753eee97163a4dbbacfbedf3e8
k.hamada <k.hamada.1988@gmail.com>
2018-04-29 01:22:12 +0900

    [rust] scrabble-score

---
+++ rust/scrabble-score/src/lib.rs
@@ -0,0 +1,22 @@
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::collections::HashMap;
+
+lazy_static! {
+    static ref SCORE_MAP: HashMap<char, u32> = {
+        [('A', 1), ('B', 3), ('C', 3), ('D', 2), ('E', 1), ('F', 4), ('G', 2),
+         ('H', 4), ('I', 1), ('J', 8), ('K', 5), ('L', 1), ('M', 3), ('N', 1),
+         ('O', 1), ('P', 3), ('Q', 10), ('R', 1), ('S', 1), ('T', 1), ('U', 1),
+         ('V', 4), ('W', 4), ('X', 8), ('Y', 4), ('Z', 10)]
+         .iter().cloned().collect()
+    };
+}
+
+pub fn score(words : &str) -> u32 {
+    words.to_string()
+        .chars()
+        .map(|char| SCORE_MAP.get(&char.to_ascii_uppercase()).unwrap_or(&0) )
+        .sum()
+}
