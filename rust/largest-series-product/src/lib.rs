7d6a070e3dd7a7aa4b6d3d63d4f82233e7ab5d69
k.hamada <k.hamada.1988@gmail.com>
2018-05-05 00:36:49 +0900

    [rust] largest-series-product

--- rust/largest-series-product/src/lib.rs
+++ rust/largest-series-product/src/lib.rs
@@ -8,17 +8,14 @@ pub fn lsp(string_digits: &str, span: usize) -> Result<u64, Error> {
     if span == 0 { return Ok(1) }
     if string_digits.len() < span { return Err(Error::SpanTooLong) }
 
-    let digits = string_digits.chars()
+    string_digits.chars()
         .map(|c| c.to_digit(10).map(|n| n as u64).ok_or(c) )
-        .collect::<Result<Vec<_>, _>>();
-
-    if digits.is_err() {
-        return Err(Error::InvalidDigit(digits.unwrap_err()));
-    };
-
-    let max = digits.unwrap()
-        .windows(span)
-        .map(|window| window.iter().product() )
-        .max();
-    return Ok(max.unwrap())
+        .collect::<Result<Vec<_>, _>>()
+        .map_err(|c| Error::InvalidDigit(c) )
+        .map(|digits|
+            digits.windows(span)
+                .map(|window| window.iter().product() )
+                .max()
+                .unwrap()
+        )
 }
