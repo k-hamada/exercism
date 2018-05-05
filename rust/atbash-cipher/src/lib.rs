5329c2433dcd13d8c4df1a22c977d4161a31aab0
k.hamada <k.hamada.1988@gmail.com>
2018-05-06 01:42:40 +0900

    [rust] atbash-cipher

--- rust/atbash-cipher/src/lib.rs
+++ rust/atbash-cipher/src/lib.rs
@@ -1,9 +1,31 @@
 /// "Encipher" with the Atbash cipher.
 pub fn encode(plain: &str) -> String {
-    unimplemented!("Encoding of {:?} in Atbash cipher.", plain);
+    plain
+        .chars()
+        .filter_map(|c| match c {
+            '0'...'9' => Some(c),
+            'A'...'z' => Some(atbash(c.to_ascii_lowercase())),
+            _ => None,
+        })
+        .collect::<Vec<_>>()
+        .chunks(5)
+        .map(|window| window.iter().collect::<String>())
+        .collect::<Vec<_>>()
+        .join(" ")
 }
 
 /// "Decipher" with the Atbash cipher.
 pub fn decode(cipher: &str) -> String {
-    unimplemented!("Decoding of {:?} in Atbash cipher.", cipher);
+    cipher
+        .chars()
+        .filter_map(|c| match c {
+            '0'...'9' => Some(c),
+            'a'...'z' => Some(atbash(c)),
+            _ => None,
+        })
+        .collect()
+}
+
+fn atbash(c: char) -> char {
+    ((25 - ((c as u8) - 97)) + 97) as char
 }
