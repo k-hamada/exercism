ea285356209d70542adf411b86c70645b4abd26d
k.hamada <k.hamada.1988@gmail.com>
2018-06-05 19:49:16 +0900

    [rust] crypto-square

--- rust/crypto-square/src/lib.rs
+++ rust/crypto-square/src/lib.rs
@@ -1,3 +1,45 @@
 pub fn encrypt(input: &str) -> String {
-    unimplemented!("Encrypt {:?} using a square code", input)
+    let chars = normalize(input).chars().collect::<Vec<_>>();
+    mapping(square(chars.len()))
+        .iter()
+        .map(|row| {
+            row.iter()
+                .map(|&n| chars.get(n).unwrap_or(&' '))
+                .collect::<String>()
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+}
+
+fn normalize(input: &str) -> String {
+    input
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .map(|c| c.to_lowercase().to_string())
+        .collect()
+}
+
+fn square(size: usize) -> (usize, usize) {
+    for c in 1.. {
+        for r in c - 1..c + 1 {
+            if c * r >= size {
+                return (c, r);
+            }
+        }
+    }
+    return (0, 0);
+}
+
+fn mapping((c, r): (usize, usize)) -> Vec<Vec<usize>> {
+    (0..c).map(|n| step_by(n, c, r)).collect()
+}
+
+fn step_by(offset: usize, step: usize, len: usize) -> Vec<usize> {
+    let mut res = Vec::with_capacity(len);
+    let mut i = 0;
+    while res.len() < len {
+        res.push(i + offset);
+        i += step;
+    }
+    res
 }
--- rust/crypto-square/tests/crypto-square.rs
+++ rust/crypto-square/tests/crypto-square.rs
@@ -11,7 +11,6 @@ fn test_empty_input() {
 }
 
 #[test]
-#[ignore]
 fn test_encrypt_also_decrypts_square() {
     // note that you only get the exact input back if:
     // 1. no punctuation
@@ -23,7 +22,6 @@ fn test_encrypt_also_decrypts_square() {
 }
 
 #[test]
-#[ignore]
 fn test_example() {
     test(
         "If man was meant to stay on the ground, god would have given us roots.",
@@ -32,13 +30,11 @@ fn test_example() {
 }
 
 #[test]
-#[ignore]
 fn test_empty_last_line() {
     test("congratulate", "crl oaa ntt gue")
 }
 
 #[test]
-#[ignore]
 fn test_spaces_are_reorganized() {
     test("abet", "ae bt");
     test("a bet", "ae bt");
@@ -46,7 +42,6 @@ fn test_spaces_are_reorganized() {
 }
 
 #[test]
-#[ignore]
 fn test_everything_becomes_lowercase() {
     test("caSe", "cs ae");
     test("cAsE", "cs ae");
@@ -54,7 +49,6 @@ fn test_everything_becomes_lowercase() {
 }
 
 #[test]
-#[ignore]
 fn test_long() {
     test(
         r#"
