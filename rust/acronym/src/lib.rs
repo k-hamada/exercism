d18e8d155e9df37797469fb167fc29e74c8aef4b
k.hamada <k.hamada.1988@gmail.com>
2018-06-24 21:45:49 +0900

    [rust] acronym

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,1 +1,17 @@
-
+pub fn abbreviate(text: &str) -> String {
+    text.chars()
+        .scan(' ', |prev, word| {
+            let ret;
+            if !prev.is_ascii_alphabetic() && word.is_ascii_alphabetic()
+                || prev.is_lowercase() && word.is_uppercase()
+            {
+                ret = Some(Some(word.to_ascii_uppercase()));
+            } else {
+                ret = Some(None);
+            }
+            *prev = word;
+            ret
+        })
+        .flat_map(|o| o)
+        .collect()
+}
