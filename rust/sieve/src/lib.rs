0d1bec52e3b48ee857096e4923218752c502bd8e
k.hamada <k.hamada.1988@gmail.com>
2018-06-30 22:29:31 +0900

    [rust] sieve

--- rust/sieve/src/lib.rs
+++ rust/sieve/src/lib.rs
@@ -1,1 +1,17 @@
+use std::collections::HashMap;
 
+pub fn primes_up_to(n: usize) -> Vec<usize> {
+    let hash_map: HashMap<usize, _> =
+        (2..n+1)
+            .flat_map(|i|
+                (2..)
+                    .map(move |x| x * i)
+                    .take_while(|x| x <= &n)
+            )
+            .map(|x| (x, ()))
+            .collect();
+            
+    (2..n+1)
+        .filter(|i| !hash_map.contains_key(i))
+        .collect()
+}
