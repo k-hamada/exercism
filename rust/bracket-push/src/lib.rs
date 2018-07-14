a03d3c5a836deafe5cb68364dc45227f0acf8c77
k.hamada <k.hamada.1988@gmail.com>
2018-07-15 01:10:50 +0900

    [rust] bracket-push

--- rust/bracket-push/src/lib.rs
+++ rust/bracket-push/src/lib.rs
@@ -1,1 +1,60 @@
+struct Bracket(char);
 
+impl Bracket {
+    pub fn is_open(&self) -> bool {
+        match self.0 {
+            '[' => true,
+            '{' => true,
+            '(' => true,
+            _ => false,
+        }
+    }
+    pub fn is_close(&self) -> bool {
+        match self.0 {
+            ']' => true,
+            '}' => true,
+            ')' => true,
+            _ => false,
+        }
+    }
+    pub fn is_pair(&self, close: &Bracket) -> bool {
+        match (self.0, close.0) {
+            ('[', ']') => true,
+            ('{', '}') => true,
+            ('(', ')') => true,
+            _ => false
+        }
+    }
+}
+
+
+pub struct Brackets(Vec<Bracket>);
+
+impl From<&'static str> for Brackets {
+    fn from(sentence: &str) -> Self {
+        Brackets(sentence.chars().map(|c| Bracket(c)).collect())
+    }
+}
+
+impl Brackets {
+    pub fn are_balanced(&self) -> bool {
+        let mut stack = Vec::new();
+
+        for bracket in self.0.iter() {
+            if bracket.is_open() {
+                stack.push(bracket);
+            }
+            else if bracket.is_close() {
+                let is_pair = {
+                    if let Some(last_open) = stack.last() {
+                        last_open.is_pair(bracket)
+                    } else { false }
+                };
+                if !is_pair { return false; }
+
+                stack.pop();
+            }
+        }
+        stack.is_empty()
+    }
+}
