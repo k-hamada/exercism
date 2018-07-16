c2dca8ce871a7970c39e3b4fa3ac6987e2568fb1
k.hamada <k.hamada.1988@gmail.com>
2018-07-16 16:32:46 +0900

    [rust] all-your-base

--- rust/all-your-base/src/lib.rs
+++ rust/all-your-base/src/lib.rs
@@ -37,10 +37,29 @@ pub enum Error {
 ///     process input with leading 0 digits.
 ///
 pub fn convert(number: &[u32], from_base: u32, to_base: u32) -> Result<Vec<u32>, Error> {
-    unimplemented!(
-        "Convert {:?} from base {} to base {}",
-        number,
-        from_base,
-        to_base
-    )
+    if number.iter().any(|n| n >= &from_base) { return Err(Error::InvalidDigit(from_base)); };
+    if from_base <= 1 { return Err(Error::InvalidInputBase) };
+    if to_base <= 1 { return Err(Error::InvalidOutputBase) };
+
+    Ok(base_conversion(decimal_conversion(number, from_base), to_base))
+}
+
+fn decimal_conversion(number: &[u32], base: u32) -> u32 {
+    number.iter()
+        .rev()
+        .enumerate()
+        .map(|(i, e)| e * base.pow(i as u32) )
+        .sum()
+}
+
+fn base_conversion(number: u32, base: u32) -> Vec<u32> {
+    let mut result = Vec::new();
+    let mut n = number;
+    while n > 0 {
+        let quotient = n / base;
+        result.push(n - (quotient * base));
+        n = quotient;
+    }
+    result.reverse();
+    result
 }
