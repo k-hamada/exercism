b98176c5ccf4b2c35a3fd52b3fc71a4fed563b45
k.hamada <k.hamada.1988@gmail.com>
2018-07-22 22:46:26 +0900

    [rust] queen-attack

--- rust/queen-attack/src/lib.rs
+++ rust/queen-attack/src/lib.rs
@@ -1,1 +1,33 @@
+pub struct ChessPosition {
+    file: i8,
+    rank: i8
+}
 
+impl ChessPosition {
+    pub fn new(file: i8, rank: i8) -> Result<ChessPosition, ()> {
+        if file < 0 || 8 <= file || rank < 0 || 8 <= rank {
+            return Err(());
+        }
+
+        Ok(ChessPosition { file, rank })
+    }
+}
+
+pub struct Queen {
+    position: ChessPosition
+}
+
+impl Queen {
+    pub fn new(position: ChessPosition) -> Queen {
+        Queen { position }
+    }
+
+    pub fn can_attack(self, other: &Queen) -> bool {
+        if self.position.file == other.position.file { return true; }
+        if self.position.rank == other.position.rank { return true; }
+        if (self.position.file - self.position.rank) == (other.position.file - other.position.rank) { return true; }
+        if (self.position.file + self.position.rank) == (other.position.file + other.position.rank) { return true; }
+
+        return false;
+    }
+}
