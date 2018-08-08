591831e275415d5ed9b27217c154db68411d529a
k.hamada <k.hamada.1988@gmail.com>
2018-08-08 19:07:01 +0900

    [rust] sublist

--- rust/sublist/src/lib.rs
+++ rust/sublist/src/lib.rs
@@ -1,1 +1,26 @@
+#[derive(Debug, PartialEq)]
+pub enum Comparison {
+    Equal,
+    Sublist,
+    Superlist,
+    Unequal
+}
 
+pub fn sublist<T: PartialEq>(a: &[T], b: &[T]) -> Comparison {
+    if a == b {
+        return Comparison::Equal
+    }
+    if is_sublist(a, b) {
+        return Comparison::Sublist;
+    }
+    if is_sublist(b, a) {
+        return Comparison::Superlist;
+    }
+    Comparison::Unequal
+}
+
+fn is_sublist<T: PartialEq>(a: &[T], b: &[T]) -> bool {
+    if b.is_empty() { return false; }
+    if b.starts_with(a) { return true; }
+    is_sublist(a, b.split_first().map_or(&[], |(_, tail)| tail))
+}
