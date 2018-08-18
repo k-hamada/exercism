f74b025184dce4c202e020d75e95bcba58dbfaff
k.hamada <k.hamada.1988@gmail.com>
2018-08-18 19:23:49 +0900

    [rust] variable-length-quantity

--- rust/variable-length-quantity/src/lib.rs
+++ rust/variable-length-quantity/src/lib.rs
@@ -6,10 +6,44 @@ pub enum Error {
 
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    unimplemented!("Convert the values {:?} to a list of bytes", values)
+    let mut res = vec![];
+    for value in values {
+        res.append(&mut to_byte(value));
+    }
+    res
+}
+
+fn to_byte(value: &u32) -> Vec<u8> {
+    let mut res = vec![];
+    res.push((value & 0x7F) as u8);
+    let mut val: u32 = value >> 7;
+    while val > 0 {
+        res.push((0x80 + (val & 0x7f)) as u8);
+        val >>= 7
+    }
+    res.reverse();
+    res
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    unimplemented!("Convert the list of bytes {:?} to a list of numbers", bytes)
+    let mut res = vec![];
+    let mut value = 0u32;
+    for byte in bytes {
+        value = value << 7 | (byte & 0x7F) as u32;
+        if byte < &0x80 {
+            res.push(value);
+            value = 0u32;
+        }
+
+        if value > u32::max_value() >> 7 {
+            return Err(Error::Overflow);
+        }
+    }
+
+    if res.is_empty() {
+        return Err(Error::IncompleteNumber);
+    }
+
+    Ok(res)
 }
