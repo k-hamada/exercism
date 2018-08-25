c0e9a0c6bb6d9030463b1b0490e9d0cef159aa3f
k.hamada <k.hamada.1988@gmail.com>
2018-08-26 00:12:40 +0900

    [rust] phone-number

--- rust/phone-number/src/lib.rs
+++ rust/phone-number/src/lib.rs
@@ -1,6 +1,24 @@
 pub fn number(user_number: &str) -> Option<String> {
-    unimplemented!(
-        "Given the number entered by user '{}', convert it into SMS-friendly format. If the entered number is not a valid NANP number, return None.",
-        user_number
-    );
+    let input_number = user_number.chars()
+        .filter_map(|c| if c.is_ascii_digit() { c.to_digit(10) } else { None } )
+        .collect::<Vec<_>>();
+
+    let phone_number =
+        if input_number.len() == 10 {
+            Some(input_number.as_slice())
+        } else if input_number.len() == 11 && input_number.get(0) == Some(&1) {
+            input_number.get(1..)
+        } else {
+            None
+        };
+
+    if let Some(number) = phone_number {
+        match number {
+            [n1, _, _, n2, _, _, _, _, _, _] if n1 >= &2 && n2 >= &2 =>
+                number.into_iter().cloned().filter_map(|i| std::char::from_digit(i, 10)).collect::<String>().into(),
+            _ => None
+        }
+    } else {
+        None
+    }
 }
