3a7d3fddb8345bbc1b16611763969fa00091c636
k.hamada <k.hamada.1988@gmail.com>
2018-09-01 03:41:17 +0900

    [rust] wordy

--- rust/wordy/src/lib.rs
+++ rust/wordy/src/lib.rs
@@ -1,16 +1,53 @@
-pub struct WordProblem;
+enum Calculate {
+    Integer(i32),
+    Plus,
+    Minus,
+    Multiplied,
+    Divided
+}
+
+impl Calculate {
+    fn new(command: &str) -> Option<Self> {
+        match (command, command.parse()) {
+            (_, Ok(n)) => Some(Calculate::Integer(n)),
+            ("plus", _) => Some(Calculate::Plus),
+            ("minus", _) => Some(Calculate::Minus),
+            ("multiplied", _) => Some(Calculate::Multiplied),
+            ("divided", _) => Some(Calculate::Divided),
+            _ => None,
+        }
+    }
+}
+
+pub struct WordProblem {
+    commands: Vec<Calculate>
+}
 
 impl WordProblem {
     pub fn new(command: &str) -> Self {
-        unimplemented!(
-            "From the '{}' command construct a new WordProblem struct.",
-            command
-        );
+        WordProblem {
+            commands: command.trim_left_matches("What is ")
+                .trim_right_matches('?')
+                .split_whitespace()
+                .filter_map(|s| Calculate::new(s))
+                .collect::<Vec<_>>()
+        }
     }
 
     pub fn answer(&self) -> Option<i32> {
-        unimplemented!(
-            "Return the result of the transmitted command or None, if the command is invalid."
-        );
+        match self.commands.split_first() {
+            Some((_, [])) => None,
+            Some((&Calculate::Integer(i), commands)) =>
+                commands.chunks(2).fold(Some(i), |acc, chunk| {
+                    match chunk {
+                        [Calculate::Plus, Calculate::Integer(n)] => acc.map(|m| m + n),
+                        [Calculate::Minus, Calculate::Integer(n)] => acc.map(|m| m - n),
+                        [Calculate::Multiplied, Calculate::Integer(n)] => acc.map(|m| m * n),
+                        [Calculate::Divided, Calculate::Integer(n)] => acc.map(|m| m / n),
+                        _ => None
+                    }
+                }),
+            _ => None
+        }
     }
 }
