8e2713de7b4903afad1e7c572afa2bc2c8a62e05
k.hamada <k.hamada.1988@gmail.com>
2018-09-24 18:47:30 +0900

    [rust] two-bucket

--- rust/two-bucket/src/lib.rs
+++ rust/two-bucket/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::VecDeque;
+use std::collections::HashSet;
+
 #[derive(PartialEq, Eq, Debug)]
 pub enum Bucket {
     One,
@@ -18,11 +21,45 @@ pub struct BucketStats {
 
 /// Solve the bucket problem
 pub fn solve(capacity_1: u8, capacity_2: u8, goal: u8, start_bucket: &Bucket) -> BucketStats {
-    unimplemented!(
-        "Given one bucket of capacity {}, another of capacity {}, starting with {:?}, find pours to reach {}",
-        capacity_1,
-        capacity_2,
-        start_bucket,
-        goal,
-    );
+    let mut buf = VecDeque::new();
+    let mut checked = HashSet::new();
+
+    let start = match start_bucket {
+        &Bucket::One => ((capacity_1, 0), 1),
+        &Bucket::Two => ((0, capacity_2), 1),
+    };
+    buf.push_back(start);
+    match start_bucket {
+        &Bucket::One => checked.insert((0, capacity_2)),
+        &Bucket::Two => checked.insert((capacity_1, 0)),
+    };
+
+    while let Some((b, moves)) = buf.pop_front() {
+        if checked.contains(&b) { continue; }
+        checked.insert(b);
+
+        if b.0 == goal {
+            return BucketStats { moves, goal_bucket: Bucket::One, other_bucket: b.1 }
+        }
+        if b.1 == goal {
+            return BucketStats { moves, goal_bucket: Bucket::Two, other_bucket: b.0 }
+        }
+        buf.push_back((move_to_one(b, capacity_1), moves + 1));
+        buf.push_back((move_to_two(b, capacity_2), moves + 1));
+        buf.push_back(((b.0, 0), moves + 1));
+        buf.push_back(((0, b.1), moves + 1));
+        buf.push_back(((b.0.max(capacity_1), b.1), moves + 1));
+        buf.push_back(((b.0, b.1.max(capacity_2)), moves + 1));
+    }
+    return BucketStats { moves: 0, goal_bucket: Bucket::One, other_bucket: 0 }
+}
+
+fn move_to_one(bucket: (u8, u8), capacity_1: u8) -> (u8, u8) {
+    let space_1 = (capacity_1 - bucket.0).min(bucket.1);
+    (bucket.0 + space_1, bucket.1 - space_1)
+}
+
+fn move_to_two(bucket: (u8, u8), capacity_2: u8) -> (u8, u8) {
+    let moved = move_to_one((bucket.1, bucket.0), capacity_2);
+    (moved.1, moved.0)
 }
--- rust/two-bucket/tests/two-bucket.rs
+++ rust/two-bucket/tests/two-bucket.rs
@@ -15,7 +15,6 @@ fn test_case_1() {
 }
 
 #[test]
-#[ignore]
 fn test_case_2() {
     assert_eq!(
         solve(3, 5, 1, &Bucket::Two),
@@ -28,7 +27,6 @@ fn test_case_2() {
 }
 
 #[test]
-#[ignore]
 fn test_case_3() {
     assert_eq!(
         solve(7, 11, 2, &Bucket::One),
@@ -41,7 +39,6 @@ fn test_case_3() {
 }
 
 #[test]
-#[ignore]
 fn test_case_4() {
     assert_eq!(
         solve(7, 11, 2, &Bucket::Two),
@@ -54,7 +51,6 @@ fn test_case_4() {
 }
 
 #[test]
-#[ignore]
 fn test_case_5() {
     assert_eq!(
         solve(1, 3, 3, &Bucket::Two),
@@ -67,7 +63,6 @@ fn test_case_5() {
 }
 
 #[test]
-#[ignore]
 fn test_case_6() {
     assert_eq!(
         solve(2, 3, 3, &Bucket::One),
