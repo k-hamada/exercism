168047d7ca6dc3f695dbea583cd18eb0fc559666
k.hamada <k.hamada.1988@gmail.com>
2018-09-29 16:28:47 +0900

    [rust] pig-latin

--- rust/pig-latin/src/lib.rs
+++ rust/pig-latin/src/lib.rs
@@ -1,1 +1,36 @@
+pub fn translate(text: &str) -> String {
+    text.split_whitespace()
+        .map(|word| {
+            let words = word.to_string();
+            let (first, last) = words.split_at(split_size(word));
+            format!("{}{}ay", last, first)
+        })
+        .collect::<Vec<_>>()
+        .join(" ")
+}
 
+fn split_size(word: &str) -> usize {
+    let rules = vec![
+        ("squ", 3),
+        ("thr", 3),
+        ("sch", 3),
+        ("ch", 2),
+        ("qu", 2),
+        ("th", 2),
+        ("a", 0),
+        ("o", 0),
+        ("i", 0),
+        ("u", 0),
+        ("e", 0),
+        ("xr", 0),
+        ("yt", 0),
+    ];
+
+    for rule in rules {
+        if word.starts_with(rule.0) {
+            return rule.1;
+        }
+    }
+
+    return 1;
+}
