0d3c63da27f26395f3e48810969dad1b125ade9b
k.hamada <k.hamada.1988@gmail.com>
2018-10-21 21:02:05 +0900

    [rust] palindrome-products

--- rust/palindrome-products/src/lib.rs
+++ rust/palindrome-products/src/lib.rs
@@ -1,22 +1,32 @@
+use std::collections::HashSet;
+
 pub type Palindrome = u64;
+
 pub fn get_palindrome_products(min: u64, max: u64) -> Vec<Palindrome> {
-    unimplemented!(
-        "Find all palindromic numbers which are products of numbers in the inclusive range ({}..{})",
-        min,
-        max
-    )
+    let range = (min..max+1).collect::<Vec<_>>();
+    let products = range.iter().flat_map(|n|
+        range.iter().filter_map(move |m| {
+            let product = n * m;
+            if is_palindromic(product) { Some(product) } else { None }
+        })
+    ).collect::<HashSet<_>>();
+    products.into_iter().collect::<Vec<_>>()
 }
 
 pub fn min(palindromes: &[Palindrome]) -> Option<Palindrome> {
-    unimplemented!(
-        "Return the palindrome of minimal value from the supplied list: {:?}",
-        palindromes
-    )
+    palindromes.iter().min().map(|n| *n)
 }
 
 pub fn max(palindromes: &[Palindrome]) -> Option<Palindrome> {
-    unimplemented!(
-        "Return the palindrome of maximal value from the supplied list: {:?}",
-        palindromes
-    )
+    palindromes.iter().max().map(|n| *n)
+}
+
+fn is_palindromic(n: u64) -> bool {
+    let mut base = n;
+    let mut result = 0;
+    while base > 0 {
+        result = result * 10 + base % 10;
+        base /= 10;
+    }
+    n == result
 }
