46b3f976cfdab3d13159233481d3335bde9664b0
k.hamada <k.hamada.1988@gmail.com>
2019-04-06 14:55:48 +0900

    [rust] robot-name

--- rust/robot-name/src/lib.rs
+++ rust/robot-name/src/lib.rs
@@ -1,15 +1,29 @@
-pub struct Robot;
+use rand::seq::SliceRandom;
+
+pub struct Robot(String);
+
+const ALPHABET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const NUMBER: &[u8] = b"0123456789";
+
+fn id_generator() -> String {
+    let mut rng = &mut rand::thread_rng();
+
+    let alphabet = ALPHABET.choose_multiple(&mut rng, 2).cloned();
+    let number = NUMBER.choose_multiple(&mut rng, 3).cloned();
+    let id = alphabet.chain(number).collect();
+    String::from_utf8(id).unwrap_or_default()
+}
 
 impl Robot {
     pub fn new() -> Self {
-        unimplemented!("Construct a new Robot struct.");
+        Robot(id_generator())
     }
 
     pub fn name(&self) -> &str {
-        unimplemented!("Return the reference to the robot's name.");
+        &self.0
     }
 
     pub fn reset_name(&mut self) {
-        unimplemented!("Assign a new unique name to the robot.");
+        self.0 = id_generator();
     }
 }
