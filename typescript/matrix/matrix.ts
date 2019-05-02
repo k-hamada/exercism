b584616c71ea7472a1fb34a361c507d886bee4e9
k.hamada <k.hamada.1988@gmail.com>
2019-05-02 18:18:16 +0900

    [typescript] matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,6 +1,14 @@
 class Matrix {
-    constructor( /* Parameters go here */ ) {
-        // Your code here
+    rows: number[][];
+    columns: number[][];
+
+    constructor(input: string) {
+        this.rows = input.split(/\n/).map(i => i.split(/\s/).map(t => parseInt(t)))
+        this.columns = this.transpose(this.rows)
+    }
+
+    transpose(array: number[][]): number[][] {
+        return array[0].map((_, c) => array.map(r => r[c]))
     }
 }
 
