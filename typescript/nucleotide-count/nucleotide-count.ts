bea388f9c4d89ac0c86eafac9650f4294d0d7139
k-hamada <k.hamada.1988@gmail.com>
2019-06-28 15:53:08 +0900

    [typescript] nucleotide-count

--- typescript/nucleotide-count/nucleotide-count.ts
+++ typescript/nucleotide-count/nucleotide-count.ts
@@ -1,6 +1,19 @@
+const DefaultNucleotideCount = { A: 0, C: 0, G: 0, T: 0 }
+type NucleotideCountResult = { [key in keyof typeof DefaultNucleotideCount]: number }
+
 class NucleotideCount {
-  static nucleotideCounts( /* Parameters go here */ ) {
-    // Your code here
+  static nucleotideCounts(dna: string): NucleotideCountResult {
+    let result = Object.assign({}, DefaultNucleotideCount)
+    for (const nucleotide of dna) {
+      switch (nucleotide) {
+        case 'A': result.A += 1; break;
+        case 'C': result.C += 1; break;
+        case 'G': result.G += 1; break;
+        case 'T': result.T += 1; break;
+        default: throw new Error('Invalid nucleotide in strand')
+      }
+    }
+    return result
   }
 }
 
