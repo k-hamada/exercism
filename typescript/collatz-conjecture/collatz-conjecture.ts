a8e33dc43a6d64c960f818e7e23572a0ba0be26e
k-hamada <k.hamada.1988@gmail.com>
2019-06-28 12:56:45 +0900

    [typescript] collatz-conjecture

--- typescript/collatz-conjecture/collatz-conjecture.ts
+++ typescript/collatz-conjecture/collatz-conjecture.ts
@@ -1,6 +1,18 @@
 class CollatzConjecture {
-    static steps( /* Parameters go here */ ) {
-        // Your code here
+    static steps(n: number): number {
+        if (n <= 0) throw new Error('Only positive numbers are allowed')
+
+        let step = 0
+        while (n !== 1) {
+            step++
+            if (n % 2 === 0) {
+                n /= 2
+            } else {
+                n *= 3
+                n += 1
+            }
+        }
+        return step
     }
 }
 
